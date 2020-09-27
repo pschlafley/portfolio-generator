16a5a6296f9affe8cf442613ab5e1f760911c985
Peyton Schlafley <pschlafley0@gmail.com>
2020-09-27 13:50:44 -0400

    fixed error that wouldn't print the html onto the web browser

--- src/page-template.js
+++ src/page-template.js
@@ -6,7 +6,7 @@ const generatePage = (name, github) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo><title>
+        <title>Portfolio Demo></title>
     </head>
 
     <body>
@@ -15,7 +15,7 @@ const generatePage = (name, github) => {
             <a href="https://github.com/${github}">Github</a>
         </h2>
     </body>
-    <html>
+    </html>
     `;
 };
 
