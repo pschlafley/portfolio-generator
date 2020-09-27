665c72fde36b2ee6c3bcf8395b27a40e64b154bd
Peyton Schlafley <pschlafley0@gmail.com>
2020-09-27 15:08:42 -0400

    install inquirer and begin using in our application to get the name of the user

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+.prompt ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers))
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html',generatePage(name, github), err => {
-    if (err) throw new Error;
+// fs.writeFile('./index.html',pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Chout out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Chout out index.html to see the output!');
+// });
+
+
--- src/page-template.js
+++ src/page-template.js
@@ -6,13 +6,13 @@ const generatePage = (name, github) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo></title>
+        <title>Portfolio Demo</title>
     </head>
 
     <body>
         <h1>${name}</h1>
         <h2>
-            <a href="https://github.com/${github}">Github</a>
+            <a target="_blank" href="https://github.com/${github}">Github</a>
         </h2>
     </body>
     </html>
