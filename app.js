67d0fdbfdff99a547df11377ec52f00584926c27
Peyton Schlafley <pschlafley0@gmail.com>
2020-09-28 16:09:30 -0400

    update page template to hold more data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -133,18 +133,18 @@ const promptProject =  portfolioData => {
     });
 };
 
+
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData)
+    const pageHTML = generatePage(portfolioData);
+    
+    fs.writeFile('./index.html',pageHTML, err => {
+        if (err) throw new Error(err);
+    
+        console.log('Page created! Chout out index.html in this directory to see it!');
+    });
 })
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html',pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Chout out index.html to see the output!');
-// });
 
 
